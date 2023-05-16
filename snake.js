1a2caf90a4e29cbe6264e36fe8b2f0e71df1d0de
NoMercyPlayer <74675136+NoMercyPlayer@users.noreply.github.com>
2023-05-16 21:02:41 +0100

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,119 @@
+//board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = [];
+
+//food
+var foodX;
+var foodY;
+var foodRadius = blockSize / 2; // Radius of the food circle
+
+var gameOver = false;
+var score = 0; // Variable to store the current score
+var highScore = localStorage.getItem("highScore") || 0; // Retrieve the high score from localStorage
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    setInterval(update, 1000/10);
+}
+
+function update() {
+    if (gameOver) {
+        return;
+    }
+
+    context.fillStyle="black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle="red";
+    context.beginPath();
+    context.arc(foodX + foodRadius, foodY + foodRadius, foodRadius, 0, 2 * Math.PI); // Draw a circle for the food
+    context.fill();
+
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]);
+        placeFood();
+        score++; // Increment the score
+        if (score > highScore) {
+            highScore = score;
+            localStorage.setItem("highScore", highScore); // Store the new high score in localStorage
+        }
+    }
+
+    for (let i = snakeBody.length-1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i-1];
+    }
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+
+    context.fillStyle="cyan";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    // Draw the score and high score
+    context.fillStyle = "white";
+    context.font = "20px Arial";
+    context.fillText("Score: " + score, 10, 20);
+    context.fillText("High Score: " + highScore, 10, 40);
+
+    // game over options
+    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
+        gameOver = true;
+        alert("Game Over");
+        location.reload();
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            alert("Game Over");
+            location.reload();
+        }
+    }
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
